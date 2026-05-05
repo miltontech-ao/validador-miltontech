7d0222dddf7528993410d2ce85dd532a3d8d8e20
miltontech-ao <miltonbongo10@gmail.com>
2026-05-05 02:15:32 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'milton-tech-v5.4';
+const CACHE_NAME = 'milton-tech-v6.0'; // MUDEI A VERSÃO
 const urlsToCache = [
   './',
   './index.html',
@@ -7,13 +7,31 @@ const urlsToCache = [
   './manifest.json'
 ];
 
+// Instala e força atualizar
 self.addEventListener('install', event => {
+  self.skipWaiting(); // Força o novo SW a ativar
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
 });
 
+// Ativa e limpa cache velho
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key); // Apaga cache antigo
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+// Busca
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
