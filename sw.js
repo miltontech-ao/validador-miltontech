c70a0e236af184e4c840262ddc2c3e83ac6f8aa4
miltontech-ao <miltonbongo10@gmail.com>
2026-05-05 03:32:18 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'milton-pfs-v6.3';
+const CACHE_NAME = 'milton-pfs-v6.7';
 const urlsToCache = [
   './',
   './index.html',
@@ -7,19 +7,17 @@ const urlsToCache = [
   './icon-512.png'
 ];
 
-// Instala o Service Worker e guarda os arquivos no cache
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-     .then(cache => {
-        console.log('Cache v6.3 aberto');
+ .then(cache => {
+        console.log('Cache v6.7 aberto');
         return cache.addAll(urlsToCache);
       })
   );
   self.skipWaiting();
 });
 
-// Ativa o novo Service Worker e apaga caches antigos
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
@@ -36,16 +34,13 @@ self.addEventListener('activate', event => {
   self.clients.claim();
 });
 
-// Intercepta as requisições: se tiver no cache, usa. Se não, busca na rede
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-     .then(response => {
-        // Cache hit - retorna a resposta
+ .then(response => {
         if (response) {
           return response;
         }
-        // Não tem no cache - busca na rede
         return fetch(event.request);
       }
     )
